c4abb1452d8388c93677d505e962083e28a09b6a
Haris SKIKDI <hskikdi@e2r3p12.clusters.42paris.fr>
2023-02-21 17:00:35 +0100

    init frontend & backend

---
+++ frontend/src/ping.js
@@ -0,0 +1,14 @@
+async function pingBackend()
+{
+    try {
+        const response = await fetch("http://localhost:3000");
+        const text = await response.text();
+        console.log(text);
+
+    }
+    catch (error){
+        console.error("Error pinging back", error);
+    }
+}
+
+export default pingBackend;
