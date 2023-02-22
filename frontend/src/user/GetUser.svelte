1c1956e01bae0b262c0b2707bfaac7d9dcb8201f
ali-lab <li.arthur101@gmail.com>
2023-02-22 16:31:58 +0100

    suppression dossiers .vscode et scripts + correction sur getUser

--- frontend/src/user/GetUser.svelte
+++ frontend/src/user/GetUser.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
     import axios from "axios";
 
+    let username: string;
+
     function getUser() {
-        axios.get('http://localhost:3000/user')
+        axios.get(`http://localhost:3000/${username}`)
             .then(res => {
                 console.log(res)
-                })
+            })
             .catch(err => {
-                    console.log(err)   
-                })
+                console.log(err)   
+            })
         }
 </script>
 
 <form on:submit|preventDefault={getUser}>
-    <input type="text" placeholder="enter a username">
+    <input type="text" bind:value={username} placeholder="enter a username">
     <button type="submit">get user</button>
 </form>
 
