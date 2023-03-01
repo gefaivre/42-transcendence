5c2e2a257fc92285f2b1b418ff5a8d3dc11e00a0
ali-lab <li.arthur101@gmail.com>
2023-03-01 16:50:44 +0100

    ajout du CRUD

---
+++ backend/src/matchs/matchs.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Match } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateMatchDto } from './dto/create-match.dto';
+import { UpdateMatchDto } from './dto/update-match.dto';
+
+@Injectable()
+export class MatchsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createMatchDto: CreateMatchDto) {
+    return this.prisma.match.create({
+      data: {
+        winnerId: createMatchDto.winnerId,
+        winnerScore: createMatchDto.winnerScore,
+        loserId: createMatchDto.loserId,
+        loserScore: createMatchDto.loserScore,
+        date: createMatchDto.date,
+        ranked: createMatchDto.ranked,
+      },
+    })
+  }
+
+  async findAll() {
+    const matchs = await this.prisma.match.findMany({});
+    return matchs;
+  }
+
+  async findOne(id: number) {
+    const match = await this.prisma.match.findUnique({
+      where: {
+        id: id,
+      }
+    });
+    return match;
+  }
+
+  async update(id: number, updateMatchDto: UpdateMatchDto) {
+    return this.prisma.match.update({
+      where: { id: id },
+      data: updateMatchDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.match.delete({ where: { id: id }});
+  }
+
+  async findHistory(userId: number) {
+    const history = await this.prisma.match.findMany({
+      where: { 
+        OR: [
+          { winnerId: userId },
+          { loserId: userId },
+        ]
+      },
+      orderBy: { date: 'desc' }
+    })
+    return history;
+  }
+
+  async removeHistory(userId: number) {
+    return this.prisma.match.delete({
+      where: {
+        OR: [
+          { winnerId: userId },
+          { loserId: userId },
+        ]
+      }
+    });
+  }
+
+
+
+}
