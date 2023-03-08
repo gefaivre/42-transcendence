32ea2f03f7af431633aa811044fd8b390561b809
gefaivre <gefaivre@student.42.fr>
2023-03-08 19:31:39 +0100

    delete signup.svelte

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,6 @@ import NotFound from './routes/NotFound.svelte'
 import Home from './routes/Home.svelte'
 import UserCRUD from './routes/UserCRUD.svelte'
 import Leaderboard from './routes/Leaderboard.svelte'
-import SignUp from './routes/SignUp.svelte'
 import Users from './routes/Users.svelte'
 import Connection from './routes/Connection.svelte'
 
@@ -10,7 +9,6 @@ export default {
     '/': Home,
     '/UserCRUD': UserCRUD,
     '/leaderboard': Leaderboard,
-    '/signup': SignUp,
     '/connection': Connection,
     '/users/:name': Users,
     '*': NotFound
