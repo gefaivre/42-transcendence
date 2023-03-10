922c55062a4845037c949cd774ccb5aa7c2e141c
thoo-ma <trobin@student.42.fr>
2023-03-10 22:00:55 +0100

    jwt is now http-only cookie

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -2,38 +2,34 @@
 
     import axios from "axios";
     import { onMount } from "svelte";
-    import { getCookie, deleteCookie } from 'svelte-cookie';
     import { logged } from "../stores";
 
-    let jwt: string = getCookie('jwt')
-    let profile = { username: '', userId: '' }
+    let user = { username: '', userId: '' }
 
-    onMount(() => {
-        if (jwt) {
-            logged.update(() => true)
-            getProfile()
-        } else {
-            logged.update(() => false)
-            resetProfile()
-        }
-    })
+    onMount(() => login())
 
-    function getProfile() {
-        axios.get('http://localhost:3000/profile', {
-            headers : { Authorization: 'Bearer ' + jwt }
+    function login() {
+        axios.get('http://localhost:3000/auth/login', {
+            withCredentials: true
         })
-        .then((res) => { profile = res.data })
-        .catch((err) => { console.log(err) })
-    }
-
-    function resetProfile() {
-        profile.username = ''
-        profile.userId = ''
+        .then((res) => {
+            user = res.data
+            logged.update(() => true)
+        })
+        .catch((err) => {
+            console.log(err)
+            user.username = ''
+            user.userId = ''
+            logged.update(() => false)
+        })
     }
 
     function logout() {
-        logged.update(() => false)
-        deleteCookie('jwt')
+        axios.get('http://localhost:3000/auth/logout', {
+            withCredentials: true
+        })
+        .then(() => logged.update(() => false))
+        .catch((err) => console.log(err))
     }
 
 </script>
@@ -50,10 +46,7 @@
         <button on:click={logout}>logout</button>
         <br>
         <br>
-        <p>You successfully authenticated as <b>{profile.username}</b></p>
-        <br>
-        <p>Your jwt is:</p>
-        <p class="break">{jwt}</p>
+        <p>You successfully authenticated as <b>{user.username}</b></p>
     {:else}
         <a href={FT_AUTHORIZE} style="font-size: 30px;">Signup with 42</a>
     {/if}
