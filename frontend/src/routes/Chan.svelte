d687355dfe9bb14bad8b261b3843253d2dbeaa91
haris <hkidds23@gmail.com>
2023-04-11 17:09:42 +0200

    juste un debut de prompt pour poster sur le channels

--- frontend/src/routes/Chan.svelte
+++ frontend/src/routes/Chan.svelte
@@ -43,6 +43,16 @@
     </div>
     <div class="Wall">
     </div>
+    <div class="prompt">
+        <form>
+            <label for="chat" class="sr-only">Your message</label>
+            <textarea id="chat" rows="1" class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+            <button type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-white">
+              <svg aria-hidden="true" class="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
+              <span class="sr-only">Send message</span>
+            </button>
+          </form> 
+    </div>
 
 </Layout>
 {:else}
@@ -51,13 +61,32 @@
 
 <style>
     .Wall{
-        height: calc(100% - 56px);
+        height: calc(100% - 156px);
         width: calc(100% - 448px);
         position: absolute;
         left: 448px;
         top: 56px;
-        background-color: white;
             }
+    .prompt{
+        position: absolute;
+        width: calc(100% - 448px);
+        height: 100px;
+        bottom: 0px;
+        left: 448px;
+
+    }
+    .prompt form {
+  display: flex;
+  justify-content: space-between;
+}
+.prompt button[type="submit"] {
+  width: 100px;
+}
+.prompt button svg {
+  width: 40px;
+  height: 40px;
+}
+
     .horizontalBar{
         height: 56px;
         width: calc(100% - 448px);
