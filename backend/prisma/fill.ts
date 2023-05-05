abf064bcfe0987eb6e2a5e8a38f79bb62e8a7639
gefaivre <gefaivre@student.42.fr>
2023-05-05 15:07:01 +0200

    filling script 40%

---
+++ backend/prisma/fill.ts
@@ -0,0 +1,90 @@
+import { PrismaClient, User } from '@prisma/client'
+
+const n = 5
+let tab: User[];
+const prisma = new PrismaClient()
+
+
+function randomUsername(): string {
+		let result = '-t-';
+		const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		const numbers = '0123456789';
+		let usernameSize = Math.floor(Math.random() * (10 - 1) + 1);
+		for(let i:number = 0; i < usernameSize; i++) {
+			result += characters.charAt(Math.floor(Math.random() * characters.length));
+		}
+		for(let i:number = 0; i < 2; i++) {
+			result += numbers.charAt(Math.floor(Math.random() * numbers.length));
+		}
+		return result;
+  }
+
+function randomString(): string {
+  return Math.random().toString(36).slice(2).substring(0,7)
+}
+
+function randomNumber(): number {
+  return Math.floor(Math.random() * (150 - 0) + 0)
+}
+
+function createpending(user: User) {
+  prisma.user.update({
+    where: {
+      id: user.id
+    },
+    data: {
+      pendingFriends: {
+        connect: { id: friendId }
+      }
+    }
+  })
+}
+
+
+function createRequest(user: User) {
+
+}
+
+function createFriends(user: User) {
+
+}
+
+
+async function main() {
+  // User creation
+  for (let i = 0; i < n; i++) {
+    let user: User;
+    try {
+      user = await prisma.user.create({
+        data: {
+          username: randomUsername(),
+          password: '',
+          games: randomNumber(),
+          mmr: randomNumber(),
+					images: {
+						create: {
+              name: "default",
+              link: "/app/images/basic_pp.jpg",
+            }
+					}
+        }
+      })
+      tab.push(user)
+    } catch (error) {
+    }
+  }
+  //Connecct friends
+  for (let i = 0; i < tab.length - 1; i + 2) {
+
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
