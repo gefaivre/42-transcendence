424967590cf485ee6db336458ff8e3ec87fe7422
Thomas Robin <trobin@student.42.fr>
2023-05-22 12:40:16 +0200

    remove unused imports

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,13 +1,8 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ImagesModule } from 'src/images/images.module';
-import { AuthService } from 'src/auth/auth.service';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { AuthModule } from 'src/auth/auth.module';
-import { ImagesService } from 'src/images/images.service';
 
 @Module({
   imports: [forwardRef(() => ImagesModule)],
