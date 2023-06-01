2ac7910eda1900577a6511815bf24b960085b1a4
thoo-ma <51019002+thoo-ma@users.noreply.github.com>
2023-06-01 14:47:48 +0200

    Images (#86)
    * add image validation pipe
    
    * routing
    
    * variables name
    
    * delete image
    
    * 10b was short

--- backend/src/images/images.controller.ts
+++ backend/src/images/images.controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Get, Post, Param, Delete, UseInterceptors, UploadedFile, Req, UseGuards, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Param, Delete, UseInterceptors, UploadedFile, Req, UseGuards, ParseIntPipe, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { AuthGuard } from '@nestjs/passport';
 import * as fs from 'fs'
-
-// https://cdn.intra.42.fr/users/db271b9343eac0fdebb3e9fb79b586cc/small_gefaivre.jpg
+import { FileSizeValidationPipe } from 'src/pipes';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('images')
@@ -13,13 +12,11 @@ export class ImagesController {
 
   constructor(private readonly images: ImagesService) {}
 
-  @Post('add')
+  @Post()
   @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({
-      destination: (request, file, cb) => {
-        if (request.user === undefined || request.user === null)
-          return "You shall not pass";
-        const directory  = `/app/images/${request.user}`
+      destination: (request: any, file, cb) => {
+        const directory  = `/app/images/${request.user.username}`
         if (fs.existsSync(directory) === false)
           fs.mkdirSync(directory);
         cb(null, directory);
@@ -29,12 +26,12 @@ export class ImagesController {
       }
     })
   }))
-  addImage(@Req() request: any, @UploadedFile() file: Express.Multer.File) {
+  addImage(@Req() request: any, @UploadedFile(FileSizeValidationPipe) file: Express.Multer.File) {
     return this.images.addImage(request.user.id, file);
   }
 
-  @Get('set/:id')
-  setImage(@Req() request: any, @Param('id', ParseIntPipe) imageId: number) {
+  @Patch(':imageId')
+  setImage(@Req() request: any, @Param('imageId', ParseIntPipe) imageId: number) {
     return this.images.setImage(request.user.id, imageId);
   }
 
@@ -43,18 +40,18 @@ export class ImagesController {
     return this.images.getImage(userId);
   }
 
-  @Get('all')
+  @Get()
   findUserAll(@Req() request: any) {
     return this.images.findAll(request.user.id);
   }
 
-  @Get(':id')
-  findUserOne(@Req() request: any, @Param('id', ParseIntPipe) id: number) {
-    return this.images.findOne(request.user.id, id);
+  @Get(':imageId')
+  findUserOne(@Req() request: any, @Param('imageId', ParseIntPipe) imageId: number) {
+    return this.images.findOne(request.user.id, imageId);
   }
 
-  @Delete(':id')
-  removeOne(@Param('id', ParseIntPipe) id: number) {
-    return this.images.remove(id);
+  @Delete(':imageId')
+  removeOne(@Param('imageId', ParseIntPipe) imageId: number) {
+    return this.images.remove(imageId);
   }
 }
